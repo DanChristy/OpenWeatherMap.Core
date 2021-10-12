29a0fc6c7c773f7f16cfb1334c0b5de8f41dd4a4
SquallHall <squallhalle@gmail.com>
2021-10-12 15:56:40 +0200

    added naiv city query methode

--- OpenWeatherMap.Core/Interfaces/IOpenWeatherMap.cs
+++ OpenWeatherMap.Core/Interfaces/IOpenWeatherMap.cs
@@ -4,5 +4,6 @@ using System.Threading.Tasks;
 namespace OpenWeatherMap.Core.Interfaces {
 	public interface IOpenWeatherMap {
 		Task<T> QueryAsync<T>(double lat, double lon, Language language = Language.English, Units units = Units.Imperial, string version = "2.5");
+		Task<T> QueryAsync<T>(string cityName, Language language = Language.English, Units units = Units.Imperial, string version = "2.5");
 	}
 }
